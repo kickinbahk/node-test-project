fc58f14e6c039eec6ed11d876333f8e68de9d929
kickinbahk <kickinbahk@gmail.com>
2016-03-04 18:37:44 -0800

    comply with js standard

--- spec/greetSpec.js
+++ spec/greetSpec.js
@@ -1,12 +1,14 @@
+/* eslint-env jasmine */
+
 'use strict'
 
 var greet = require('../src/greet')
 
 describe('greet', function () {
-  it(`should greet the given name`, function () {
-    expect(greet(`Joe`)).toEqual(`Hello, Joe!`)
+  it('should greet the given name', function () {
+    expect(greet('Joe')).toEqual('Hello, Joe!')
   })
-  it(`should greet no-one special if no name given`, function () {
-    expect(greet()).toEqual(`Hello, World`)
+  it('should greet no-one special if no name given', function () {
+    expect(greet()).toEqual('Hello, World!')
   })
 })
---
+++ src/greet.js
@@ -0,0 +1,8 @@
+'use strict'
+var greet = function (name) {
+  if (name === undefined) {
+    name = 'World'
+  }
+  return `Hello, ${name}!`
+}
+module.exports = greet
